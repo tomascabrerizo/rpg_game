6dacddb19fd8b8ae6b9ab38bc2f64be69fa66cc4
tomascabrerizo <tomascabrerizo97@gmail.com>
2021-05-27 19:09:06 -0300

    try to sync dsound circular buffer [FAIL]

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set compilerFlags=-EHa- -Zi -Od -FC -WX -W4 -wd4456 -wd4201 -wd4100 -wd4189 User32.lib Gdi32.lib
+set compilerFlags=-EHa- -Zi -Od -FC -WX -W4 -wd4505 -wd4456 -wd4201 -wd4100 -wd4189 User32.lib Gdi32.lib Winmm.lib
 
 IF NOT EXIST ..\..\build mkdir ..\..\build
 pushd ..\..\build
--- code/win32_rpg.cpp
+++ code/win32_rpg.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <Dsound.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <math.h>
 
 typedef uint8_t  u8;
@@ -35,6 +36,8 @@ global LPDIRECTSOUNDBUFFER globalSecondaryBuffer;
 global DWORD globalSamplesPerSecond = 44100;
 global int globalBytesPerSample = sizeof(i16)*2;
 global int globalSecondaryBufferSize = globalSamplesPerSecond * globalBytesPerSample;
+
+global i64 globalPerformanceFrequency;
 
 #define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPGUID lpGuid, LPDIRECTSOUND* ppDS, LPUNKNOWN  pUnkOuter )
 typedef DIRECT_SOUND_CREATE(DSoundCreate);
@@ -89,11 +92,7 @@ Win32WriteSineWave()
                     tSine -= (2.0f*PI32);
                 }
             }
-
-            if(globalSecondaryBuffer->Unlock(audioArea1, audioArea1Bytes, audioArea2, audioArea2Bytes) == DS_OK)
-            {
-                int breakHere = 0;
-            }
+            globalSecondaryBuffer->Unlock(audioArea1, audioArea1Bytes, audioArea2, audioArea2Bytes);
         }
     }
 }
@@ -117,7 +116,7 @@ Win32LoadDirectSound(HWND window, DWORD samplesPerSecond, DWORD bufferSize)
                 waveFormat.nChannels = 2;
                 waveFormat.nSamplesPerSec = samplesPerSecond;
                 waveFormat.wBitsPerSample = 16;
-                waveFormat.nBlockAlign = (waveFormat.nChannels * waveFormat.wBitsPerSample/8);
+                waveFormat.nBlockAlign = (waveFormat.nChannels * (waveFormat.wBitsPerSample/8));
                 waveFormat.nAvgBytesPerSec = (waveFormat.nSamplesPerSec * waveFormat.nBlockAlign);
 
                 DSBUFFERDESC primaryBufferDescription = {};
@@ -205,8 +204,23 @@ Win32UpdateBackBuffer(HDC deviceContext, void *backBuffer, int width, int height
     );
 }
 
-internal
-LRESULT CALLBACK Win32WindowsProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+internal LARGE_INTEGER
+Win32GetWallClock()
+{
+    LARGE_INTEGER result;
+    QueryPerformanceCounter(&result);
+    return result;
+}
+
+internal r32
+Win32GetSecondsElapsed(LARGE_INTEGER start, LARGE_INTEGER end)
+{
+    r32 result = ((r32)(end.QuadPart - start.QuadPart) / (r32)globalPerformanceFrequency);
+    return result;
+}
+
+internal LRESULT CALLBACK 
+Win32WindowsProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 
     LRESULT result = 0;
@@ -234,7 +248,45 @@ LRESULT CALLBACK Win32WindowsProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
     return result;
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+internal void
+Win32DebugDrawRect(int x, int y, int width, int height, u32 color)
+{
+    int minX = x;
+    int minY = y;
+    int maxX = x + width;
+    int maxY = y + height;
+
+    if(minX < 0)
+    {
+        minX = 0;
+    }
+    if(maxX > backBufferWidth)
+    {
+        maxX = backBufferWidth;
+    }
+    if(minY < 0)
+    {
+        minY = 0;
+    }
+    if(maxY > backBufferHeihgt)
+    {
+        maxY = backBufferHeihgt;
+    }
+    
+    u8 *row = (u8 *)globalBackBuffer + (minY * backBufferPitch);
+    for(int dy = minY; dy < maxY; ++dy)
+    {
+        u32 *pixel = (u32 *)row + minX;
+        for(int dx = minX; dx <maxX; ++dx)
+        {
+            *pixel++ = color; 
+        }
+        row += backBufferPitch;
+    }
+}
+
+int CALLBACK 
+WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
     WNDCLASSA win32WindowClass = {};
     win32WindowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
@@ -269,11 +321,33 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
         HDC deviceContext = GetDC(window);
         
         Win32LoadDirectSound(window, globalSamplesPerSecond, globalSecondaryBufferSize);
-        
-        Win32WriteSineWave();
         globalSecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
-
         Win32CreateBackBuffer(&globalBackBuffer, backBufferWidth, backBufferHeihgt);
+       
+        // NOTE(tomi): Set Window Sleep granularity to 1 millisecond
+        UINT DesiredSchedulerMS = 1;
+        b32 SleepIsGranular = (timeBeginPeriod(DesiredSchedulerMS) == TIMERR_NOERROR);
+
+
+        LARGE_INTEGER performanceFrequency;
+        QueryPerformanceFrequency(&performanceFrequency);
+        globalPerformanceFrequency = performanceFrequency.QuadPart;
+
+        int monitorHz = 30;
+        r32 targetSecPerFrame = 1.0f / (r32)monitorHz;
+        LARGE_INTEGER lastTime;
+        QueryPerformanceCounter(&lastTime);
+
+        DWORD runningSampleIndex = 0;
+        DWORD lastWriteCursor = 0;
+        b32 firstSoundWrite = true;
+#define DEBUG_FRAMES 30
+        int byteToLockPosition[DEBUG_FRAMES] = {};
+        int bytesToWriteWidth[DEBUG_FRAMES] = {};
+        int writeCursorPosition[DEBUG_FRAMES] = {};
+        int playCursorPosition[DEBUG_FRAMES] = {};
+        int debugIndex = 0;
+
         
         globalRunning = true;
         while(globalRunning)
@@ -284,9 +358,132 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
                 TranslateMessage(&message); 
                 DispatchMessage(&message); 
             }
+           
+            r32 tSine = 0;
+            int toneHz = 512;
+            int wavePeriod = globalSamplesPerSecond / toneHz;
+            int volume = 500;
+        
+            DWORD playCursor;
+            DWORD writeCursor;
+            if(globalSecondaryBuffer->GetCurrentPosition(&playCursor, &writeCursor) == DS_OK)
+            {
+                if(firstSoundWrite)
+                {
+                    runningSampleIndex = writeCursor / globalBytesPerSample;
+                    firstSoundWrite = false;
+                }
+                //DWORD bytesToWrite =  (DWORD)((globalSamplesPerSecond * globalBytesPerSample) * targetSecPerFrame)*1;
+                DWORD byteToLock = (runningSampleIndex * globalBytesPerSample) % globalSecondaryBufferSize;
+                DWORD bytesToWrite = 0; 
+                if(byteToLock > playCursor)
+                {
+                    bytesToWrite = globalSecondaryBufferSize - byteToLock;
+                    bytesToWrite += playCursor;
+                }
+                else if(byteToLock < playCursor)
+                {
+                    bytesToWrite = playCursor - byteToLock;
+                }
+ 
+                VOID *audioArea1;
+                DWORD audioArea1Bytes;
+                VOID *audioArea2;
+                DWORD audioArea2Bytes;
+                if(globalSecondaryBuffer->Lock(byteToLock, bytesToWrite, 
+                    &audioArea1, &audioArea1Bytes, 
+                    &audioArea2, &audioArea2Bytes, 
+                    0) == DS_OK)
+                {
+                    i16 *byteSample = (i16 *)audioArea1;
+                    DWORD audioArea1Samples = (audioArea1Bytes / globalBytesPerSample);
+                    for(DWORD areaIndex = 0; areaIndex < audioArea1Samples; ++areaIndex)
+                    {
+                        *byteSample++ = (i16)(sinf(tSine) * volume);
+                        *byteSample++ = (i16)(sinf(tSine) * volume);
+
+                        tSine += 2.0f*PI32*(1.0f/(r32)wavePeriod);
+                        if(tSine > 2.0f*PI32)
+                        {
+                            tSine -= (2.0f*PI32);
+                        }
+                        ++runningSampleIndex;
+                    }
+
+                    byteSample = (i16 *)audioArea2;
+                    DWORD audioArea2Samples = (audioArea2Bytes / globalBytesPerSample);
+                    for(DWORD areaIndex = 0; areaIndex < audioArea2Samples; ++areaIndex)
+                    {
+                        *byteSample++ = (i16)(sinf(tSine) * volume);
+                        *byteSample++ = (i16)(sinf(tSine) * volume);
+
+                        tSine += 2.0f*PI32*(1.0f/(r32)wavePeriod);
+                        if(tSine > 2.0f*PI32)
+                        {
+                            tSine -= (2.0f*PI32);
+                        }
+                        ++runningSampleIndex;
+                    }
+                    
+                    globalSecondaryBuffer->Unlock(audioArea1, audioArea1Bytes, audioArea2, audioArea2Bytes);
+                }
+                
+                // TODO: Define a DEBUG flag for this kind of code
+                int secondaryBufferHeight = 200;
+                // NOTE: This ratio maps the secondary buffer size in bytes to the screen width
+                r32 ratio = (r32)(backBufferWidth - 2*80) / (r32)globalSecondaryBufferSize;
+                int secondaryBufferWidth = (int)(ratio * (r32)globalSecondaryBufferSize);
+
+                byteToLockPosition[debugIndex] =  (int)(ratio * (r32)byteToLock);
+                bytesToWriteWidth[debugIndex] = (int)(ratio * (r32)bytesToWrite);
+                writeCursorPosition[debugIndex] = (int)(ratio * (r32)writeCursor);
+                playCursorPosition[debugIndex] = (int)(ratio * (r32)playCursor);
+                
+                Win32DrawBackBufferPatron(globalBackBuffer, backBufferWidth, backBufferHeihgt);
+                Win32DebugDrawRect(80, 200, secondaryBufferWidth, secondaryBufferHeight, 0xFF999999);
+                
+                for(int index = 0; index < DEBUG_FRAMES; ++index)
+                {
+                    Win32DebugDrawRect(80 + byteToLockPosition[index], 220, bytesToWriteWidth[index]  , 160, 0xFFFFFFFF);
+                    //Win32DebugDrawRect(80 + byteToLockPosition[index], 220, 10, 160, 0xFF101010);
+                    //Win32DebugDrawRect(80 + writeCursorPosition[index], 220, 10, 160, 0xFF00FF00);
+                    //Win32DebugDrawRect(80 + playCursorPosition[index], 220, 10, 160, 0xFFFF0000);
+                }
+                if(++debugIndex == DEBUG_FRAMES)
+                {
+                    debugIndex = 0;
+
+                    for(int index = 0; index < DEBUG_FRAMES; ++index)
+                    {
+                        byteToLockPosition[index] = {};
+                        bytesToWriteWidth[index] = {};
+                        writeCursorPosition[index] = {};
+                        playCursorPosition[index] = {};
+                    }
+                }
+
+            }
+
             
-            Win32DrawBackBufferPatron(globalBackBuffer, backBufferWidth, backBufferHeihgt);
+            // TODO: Probably get the target secons per frame from this calculation
+            LARGE_INTEGER currentTime = Win32GetWallClock();
+            r32 currentSecPerFrame = Win32GetSecondsElapsed(lastTime, currentTime); 
+            
+            if(currentSecPerFrame < targetSecPerFrame)
+            {
+                DWORD msToSleep = (DWORD)((targetSecPerFrame - currentSecPerFrame) * 1000.0f);
+                Sleep(msToSleep);
+                QueryPerformanceCounter(&currentTime);
+                currentSecPerFrame = Win32GetSecondsElapsed(lastTime, currentTime); 
+            }
+            lastTime.QuadPart = currentTime.QuadPart;
+            
             Win32UpdateBackBuffer(deviceContext, globalBackBuffer, backBufferWidth, backBufferHeihgt);
+#if 0
+            char Buffer[64];
+            sprintf_s(Buffer, "ms: %f\n", currentSecPerFrame*1000.0f);
+            OutputDebugString(Buffer);
+#endif
         }
 
         ReleaseDC(window, deviceContext);
